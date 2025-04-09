0f9fd46a40354c8c9571291d777806bb8099a849
khushi chachapara <161737255+khushichachapara@users.noreply.github.com>
2025-04-09 12:56:42 +0530

    Delete javascript_project directory

--- javascript_project/Digital_clock/script.js
+++
@@ -1,7 +0,0 @@
-const clock = document.querySelector("#clock");
-
-setInterval(function () { 
-    let time = new Date()
-     //console.log(time.toLocaleTimeString());
-    clock.innerHTML = time.toLocaleTimeString();
-},1000);
